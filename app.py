6d6d71baaf75e31e7f65aea0899cf212590fad45
Yohana <yohanadengyd@gmail.com>
2026-09-11 00:44:57 +0300

    Apply ML-SWQM-DSS branding

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import joblib
 import numpy as np
 import pandas as pd
 import streamlit as st
+st.logo("static/images/logo.png", size="large")
 import plotly.express as px
 import plotly.graph_objects as go
 
